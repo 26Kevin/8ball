9f1106ca8bb093a24774b5e0a4ff72c017651044
26Kevin <k.patel46@share.epsb.ca>
2022-11-03 11:20:47 -0600

    still

---
+++ img/main.js
@@ -0,0 +1,20 @@
+let charOutputEl = document.getElementById("output");
+
+document.getElementById("ball-img").addEventListener("click", ballClicked);
+
+function ballClicked() {
+  //Input
+  let name = document.getElementById("char-in").value.toLowerCase();
+  let randNum = Math.random();
+  if (randNum < 0.2) {
+    charOutputEl.innerHTML = "Without a Doubt";
+  } else if (randNum < 0.4) {
+    charOutputEl.innerHTML = "As I see it,yes";
+  } else if (randNum < 0.6) {
+    charOutputEl.innerHTML = "Concentrate and ask again";
+  } else if (randNum < 0.8) {
+    charOutputEl.innerHTML = "Don't count on it";
+  } else {
+    charOutputEl.innerHTML = "Outlook not so good";
+  }
+}
